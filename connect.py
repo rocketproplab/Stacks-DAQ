9b307b6c3537cc48b9ef334157e830f7784d2d20
johnnysedor <jsedor25@gmail.com>
2018-01-27 14:02:50 -0800

    finished rgb connect test

--- connect.py
+++ connect.py
@@ -6,10 +6,29 @@ core = stacks.Core(host="192.168.1.49")
 analogdeck = stacks.AnalogDeck(core, bus_address=2)
 
 # Change leds for no reason
-for i in range[0,3]:
-    for j in range[0,255]:
+for i in range(0,3):
+    for j in range(16,255):
 
-        core.rgbled.set(int(("0x00FF00" + hex(j)[2:]), 16))
+        core.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+        analogdeck.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+        sleep(1 / 1000)
+
+    for j in reversed(range(16,255)):
+
+        core.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+        analogdeck.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+        sleep(1 / 1000)
+
+for i in range(0,4):
+    core.rgbled.set(core.rgbled.COLOR_GREEN)
+    analogdeck.rgbled.set(0)
+
+    sleep(100/ 1000)
+
+    core.rgbled.set(0)
+    analogdeck.rgbled.set(analogdeck.rgbled.COLOR_GREEN)
+
+    sleep(100/ 1000)
 
 
 
@@ -17,13 +36,13 @@ for i in range[0,3]:
 core.rgbled.set(core.rgbled.COLOR_GREEN)
 analogdeck.rgbled.set(analogdeck.rgbled.COLOR_GREEN)
 
-
-while 1:
-
-    # Read input from user
-    input = readline()
-
-    if(input[1] == "engage" || 1)
-        analogdeck.solenoiddrivers.engage(input[0])
-    else if (input[1] == "disengage" || 0)
-        analogdeck.solenoiddrivers.disengage(input[0])
+#
+# while 1:
+#
+#     # Read input from user
+#     input = readline()
+#
+#     if(input[1] == "engage" || 1)
+#         analogdeck.solenoiddrivers.engage(input[0])
+#     else if (input[1] == "disengage" || 0)
+#         analogdeck.solenoiddrivers.disengage(input[0])
