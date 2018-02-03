9a52428dd86daf8bd9cc751c7f242a12ca8f5598
johnnysedor <jsedor25@gmail.com>
2018-02-03 11:17:33 -0800

    changed to make it work + working on basic test

--- load_cell_test.py
+++ load_cell_test.py
@@ -18,36 +18,45 @@ for i in range(0,2):
 
         core.rgbled.set(int(("0x00FF00" + hex(j)[2:]), 16))
         analogdeck.rgbled.set(int(("0x00FF00" + hex(j)[2:]), 16))
-        sleep(1 / 1000)
+        time.sleep(1 / 1000)
 
     for j in reversed(range(16,255)):
 
         core.rgbled.set(int(("0x00FF00" + hex(j)[2:]), 16))
         analogdeck.rgbled.set(int(("0x00FF00" + hex(j)[2:]), 16))
-        sleep(1 / 1000)
+        time.sleep(1 / 1000)
 
 for i in range(16,127):
 
     core.rgbled.set(int(("0x00FF00" + hex(i)[2:]), 16))
     analogdeck.rgbled.set(int(("0x00FF00" + hex(i)[2:]), 16))
-    sleep(1 / 1000)
+    time.sleep(1 / 1000)
 # End of "startup" sequence
 
 
 
 # Set the voltage of the something
 
+
+
+analogdeck.wavegen.set_dc(3.3)
+analogdeck.wavegen.update_waveform(samplerate_hz=3, samples=[-5,-4,-3,-2,-1,0,1,2,3,4,5] * 30)
+analogdeck.wavegen.set_control(analogdeck.wavegen.MODE_WAVEFREERUN)
+# analogdeck.wavegen.set_control(analogdeck.wavegen.MODE_DC)
+
+
 # Set excitation voltage to +12V as the max on the load cell is +18V and the
 # max voltage of the ADC input is +13V and the recommended is +2.5V
-analogdeck.sourcemeter.set_sourcevoltage(2.5)
+
+# analogdeck.sourcemeter.set_sourcevoltage(2.5)
 
 # Set up ADCanalogdeck.dmm.stop
-dmm.set_channelranges(range0=dmm.RANGE_LOW_2V5)
+dmm.set_channelranges(range0=dmm.RANGE_AUTO)
 
-dmm.stream_arm(channel=0, samplerate=dmm.SAMPLERATE_10HZ, range_=dmm.RANGE_MED_25V)
+dmm.stream_arm(channel=0, samplerate=dmm.SAMPLERATE_10HZ, range_=dmm.RANGE_AUTO, timeout=10)
 
 start = time.time()
-dmm.trigger()
+dmm.stream_trigger()
 
 while (time.time() - start) < SAMPLE_TIME:
     for sample in dmm.stream():
