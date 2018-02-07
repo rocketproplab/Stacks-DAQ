fc6314293ea4598ad46b6afdd5d4fade99025a75
johnnysedor <jsedor25@gmail.com>
2018-02-06 22:10:08 -0800

    added the SUPER important necessary led startup sequencer

--- connect.py
+++ connect.py
@@ -4,23 +4,25 @@ import subinitial.stacks as stacks
 from time import sleep
 import math
 
-core = stacks.Core(host="192.168.2.49")
+LED_ITER = 255
 
+core = stacks.Core(host="192.168.2.49")
 analogdeck = stacks.AnalogDeck(core, bus_address=2)
+
 
 # Change leds for no reason
 for i in range(4):
-    for j in range(16,255):
+    for j in range(LED_ITER):
 
-        core.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
-        analogdeck.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+        core.rgbled.set(int(("0x00FF00" + "{:02x}".format(round(math.sin(j/ LED_ITER * math.pi) * 255))), 16))
+        analogdeck.rgbled.set(int(("0x00FF00" + "{:02x}".format(round(math.sin(j/ LED_ITER * math.pi) * 255))), 16))
         sleep(1 / 1000)
 
-    for j in reversed(range(16,255)):
-
-        core.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
-        analogdeck.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
-        sleep(1 / 1000)
+    # for j in reversed(range(16,255)):
+    #
+    #     core.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+    #     analogdeck.rgbled.set(int(("0x00FFFF" + hex(j)[2:]), 16))
+    #     sleep(1 / 1000)
 
 
 
