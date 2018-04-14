c0a8e6604d50cb73f90a434617bf5fa1fa19a26a
johnnysedor <jsedor25@gmail.com>
2018-04-14 11:07:04 -0700

    live plotting

---
+++ live_plot_ex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+
+layout = plot.figure()
+
+spec = gridspec.Gridspec(ncols=3, nrows=2)
+
+
+
+fig1.tight_layout()
--- turn_off_leds.py
+++ turn_off_leds.py
@@ -6,6 +6,6 @@ core = stacks.Core(host="192.168.2.49")
 
 analogdeck = stacks.AnalogDeck(core, bus_address=2)
 
-core.rgbled.set(int("0xFF00FF04",16))
+core.rgbled.set(int("0xFF00FF01",16))
 
-analogdeck.rgbled.set(int("0xFF00FF04",16))
+analogdeck.rgbled.set(int("0xFF00FF01",16))
