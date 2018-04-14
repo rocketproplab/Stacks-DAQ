24239454987f6ed4b66f921c2f291d8b60cc93d2
johnnysedor <jsedor25@gmail.com>
2018-04-14 14:21:49 -0700

    changed the layout

--- live_plot_ex.py
+++ live_plot_ex.py
@@ -5,17 +5,26 @@ import matplotlib.gridspec as gridspec
 
 
 
-layout = plt.figure(tight_layout=True)
-gs = gridspec.GridSpec(ncols=3, nrows=2)
+fig = plt.figure(tight_layout=True)
+gs = gridspec.GridSpec(nrows=1, ncols=2)
+
+left_gs =  gridspec.GridSpecFromSubplotSpec(nrows=3, ncols=1, subplot_spec=gs[0])
+right_gs = gridspec.GridSpecFromSubplotSpec(nrows=2, ncols=1, subplot_spec=gs[1])
 
 ax = []
 
-ax.append(plt.subplot(gs[0,0]))
-ax.append(plt.subplot(gs[0,1]))
-ax.append(plt.subplot(gs[0,2:]))
-ax.append(plt.subplot(gs[1,0:2]))
-ax.append(plt.subplot(gs[1, -1]))
+ax.append(fig.add_subplot(left_gs[0,0]))
+ax.append(fig.add_subplot(left_gs[1,0]))
+ax.append(fig.add_subplot(left_gs[2,0]))
+ax.append(fig.add_subplot(right_gs[0,:]))
+# ax.append(fig.add_subplot(right_gs[1,]))
 
 
+# gs.set_title('Test Stand Static Fire')
+ax[0].set_title('Load Cell #1')
+ax[1].set_title('Load Cell #2')
+ax[2].set_title('Load Cell #3')
+
+ax[3].set_title('Total Load')
 
 plt.show()
